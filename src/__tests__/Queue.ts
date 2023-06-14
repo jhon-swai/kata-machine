bec1a205beb02efe78cb9a25fe10887cb4102458
Chinaa Swai <jhonswai@gmail.com>
2023-06-14 07:11:51 +0300

    Queue algorithm

--- src/__tests__/Queue.ts
+++ src/__tests__/Queue.ts
@@ -3,19 +3,22 @@ import Queue from "@code/Queue";
 test("queue", function () {
     const list = new Queue<number>();
 
+    console.log("begining", list)
+    // list is empty
+    expect(list.peek()).toEqual(undefined)
+    
     list.enqueue(5);
     list.enqueue(7);
     list.enqueue(9);
 
+    console.log("middle", list)
+
+    expect(list.peek()).toEqual(5);
     expect(list.deque()).toEqual(5);
     expect(list.length).toEqual(2);
 
-    // this must be wrong..?
-    debugger;
-
-    // i hate using debuggers
     list.enqueue(11);
-    debugger;
+
     expect(list.deque()).toEqual(7);
     expect(list.deque()).toEqual(9);
     expect(list.peek()).toEqual(11);
@@ -25,7 +28,10 @@ test("queue", function () {
 
     // just wanted to make sure that I could not blow up myself when i remove
     // everything
+    console.log("end",list)
+
     list.enqueue(69);
+
     expect(list.peek()).toEqual(69);
     expect(list.length).toEqual(1);
 });
